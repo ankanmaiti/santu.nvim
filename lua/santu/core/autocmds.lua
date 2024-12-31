2de645ff0a68b03841f966ed47a2fb7e014fc681
Ankan maiti <ankanmaiti2@gmail.com>
2024-12-31 09:56:12 +0530

    refactor (indent): 2 space for lua

--- lua/santu/core/autocmds.lua
+++ lua/santu/core/autocmds.lua
@@ -2,25 +2,25 @@ local auBufcheck = vim.api.nvim_create_augroup('bufcheck', {clear = true})
 
 -- reload config file on change
 vim.api.nvim_create_autocmd('BufWritePost', {
-    group    = auBufcheck,
-    pattern  = vim.env.MYVIMRC,
-    command  = 'silent source %'
+  group    = auBufcheck,
+  pattern  = vim.env.MYVIMRC,
+  command  = 'silent source %'
 })
 
 -- highlight yanks
 vim.api.nvim_create_autocmd('TextYankPost', {
-    group    = auBufcheck,
-    pattern  = '*',
-    callback = function() vim.highlight.on_yank{timeout=250} end 
+  group    = auBufcheck,
+  pattern  = '*',
+  callback = function() vim.highlight.on_yank{timeout=250} end 
 })
 
 -- sync clipboards because I'm easily confused
 vim.api.nvim_create_autocmd('TextYankPost', {
-    group    = auBufcheck,
-    pattern  = '*',
-    callback = function() 
-        -- Replace 'fn' with 'vim.fn'
-        vim.fn.setreg('+', vim.fn.getreg('*'))
-    end 
+  group    = auBufcheck,
+  pattern  = '*',
+  callback = function() 
+    -- Replace 'fn' with 'vim.fn'
+    vim.fn.setreg('+', vim.fn.getreg('*'))
+  end 
 })
 
--- lua/santu/init.lua
+++ lua/santu/init.lua
@@ -17,11 +17,11 @@ require('santu.core.autocmds')
 --
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-	local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-	if vim.v.shell_error ~= 0 then
-		error('Error cloning lazy.nvim:\n' .. out)
-	end
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 --
@@ -41,16 +41,16 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-	--
-	-- Explicitly add your own plugins, configuration, etc from `lua/santu/plugins/*.lua`
-    require('santu.plugins.explorer'),
-    require('santu.plugins.colorscheme'),
-	--
-	--
-	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/santu/plugins/*.lua`
-	--    This is the easiest way to modularize your config.
-	--
-	--  Uncomment the following line and add your plugins to `lua/santu/plugins/*.lua` to get going.
-	-- { import = 'santu.plugins' },
-	--
+  --
+  -- Explicitly add your own plugins, configuration, etc from `lua/santu/plugins/*.lua`
+  require('santu.plugins.explorer'),
+  require('santu.plugins.colorscheme'),
+  --
+  --
+  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/santu/plugins/*.lua`
+  --    This is the easiest way to modularize your config.
+  --
+  --  Uncomment the following line and add your plugins to `lua/santu/plugins/*.lua` to get going.
+  -- { import = 'santu.plugins' },
+  --
 })
