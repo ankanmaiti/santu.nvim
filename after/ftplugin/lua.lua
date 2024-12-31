56dac6bc4b911f4baa4b69045049a7d24b2ce1f9
Ankan maiti <ankanmaiti2@gmail.com>
2024-12-31 09:55:14 +0530

    feat: lua specific commands

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,5 @@
+local set = vim.opt_local
+
+set.shiftwidth = 2              -- Number of spaces for indentation
+set.number = true               -- Make line numbers default
+set.relativenumber = true       -- Set relative numbered linesend
